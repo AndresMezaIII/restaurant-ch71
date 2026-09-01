c4c81b386784e9e1479e5bef2f03c2b892cd6107
Andres M <314112705+AndresMezaIII@users.noreply.github.com>
2026-09-01 16:06:07 -0500

    after assignment 2

---
+++ scripts/assignment2.js
@@ -0,0 +1,44 @@
+function showRestaurantName() {
+  console.log("The Royal Lounge");
+}
+
+showRestaurantName();
+
+function greetCustomer(customer) {
+  console.log(
+    `Hello ${customer}, we are happy to have you at The Royal Lounge!`,
+  );
+}
+
+greetCustomer("Andres Meza");
+greetCustomer("John Doe");
+greetCustomer("Jane Smith");
+
+function calculateTotal(price, tax) {
+  let taxes = price * tax;
+  let total = price + taxes;
+  return total;
+}
+
+console.log(
+  "The price of a steak dinner is $" + calculateTotal(43, 0.08) + ".",
+);
+
+console.log(
+  "The price of the vegan special is $" + calculateTotal(25, 0.08) + ".",
+);
+
+console.log(
+  "The price of the famer's fresh salad is $" +
+    calculateTotal(19.99, 0.08).toFixed(2) +
+    ".",
+);
+
+function calculateTip(total, tipPercent) {
+  let tip = total * tipPercent;
+  return tip;
+}
+
+console.log(
+  `Dish: Chicken Kebabs \nPrice: $23\nTotal: $${calculateTotal(23, 0.08)}\nTip:$${calculateTip(calculateTotal(23, 0.08), 0.15).toFixed(2)}`,
+);
