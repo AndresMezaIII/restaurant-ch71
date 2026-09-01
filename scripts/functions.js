2932278c62fa8b9992c01e213272a87b6f1170f6
Andres M <314112705+AndresMezaIII@users.noreply.github.com>
2026-08-31 21:21:59 -0500

    closing class

--- scripts/functions.js
+++ scripts/functions.js
@@ -92,3 +92,95 @@ let z = convertToSeconds(60);
 
 console.log(y);
 console.log(z);
+
+//return vs console.log()
+//v1 console
+
+function addWrong(numOne, numTwo) {
+  console.log(numOne + numTwo);
+  //no return!!
+}
+
+let resultOne = addWrong(4, 6);
+console.log(resultOne); //undefined nothing was retuned
+
+function addCorrect(numOne, numTwo) {
+  return numOne + numTwo;
+}
+
+let resultTwo = addCorrect(4, 6);
+console.log(resultTwo); //10
+
+//global vs local variables
+
+let globalVar = "I exist everywhere!";
+
+console.log(`${globalVar} including out of function scope!`);
+
+function showScope() {
+  let localVar = "I only exist in the function.";
+  console.log(`${globalVar} Even in this function!`);
+  console.log(localVar);
+}
+
+showScope();
+console.log(globalVar);
+//console.log(localVar); //localVar is not defined because it only exists in the function
+
+//write two functions that calculate the price, the taxes, and the tip
+
+function taxCalc(price, tax) {
+  let taxes = price * tax;
+  let taxed = price + taxes;
+  return taxed;
+}
+
+taxed = taxCalc(20, 0.2);
+console.log(taxed);
+
+/*function tip(price) {
+  let tipAmount = price * 0.15;
+  return tipAmount;
+}
+
+let mealCost = taxed + tipAmount;
+console.log(mealCost);*/
+
+//create two variables, one that gets the area of a rectangle and one that gets the area of a rectangle
+function recArea(height, width) {
+  let area = height * width;
+  return area;
+}
+
+console.log(recArea(3, 5));
+
+function recPerim(height, width) {
+  let perimeter = 2 * height + 2 * width;
+  return perimeter;
+}
+
+console.log(recPerim(3, 5));
+
+//fix the function
+function calculatePay(hoursWorked, hourlyRate) {
+  let gross = hoursWorked * hourlyRate;
+  let taxes = gross * 0.2;
+  let payCheck = gross - taxes;
+  return payCheck;
+}
+
+payCheck = calculatePay(40, 35);
+console.log(payCheck);
+
+//
+//error because budget is already declared
+//1000 still
+let budget = 1000;
+function spendMoney(amount) {
+  let budget = budget - amount;
+  return budget;
+}
+
+/*budget is redeclared in the above as a local, so in the operation it says budget has not been initialized, and in console.log it prints the original budget. the function is an error*/
+//console.log(spendMoney(200));
+console.log(budget);
