91b2c5e5c0a6097b6b80c52e2db581f0c02b39b3
Andres M <314112705+AndresMezaIII@users.noreply.github.com>
2026-09-01 16:11:42 -0500

    file transfer

---
+++ scripts/assignment1.js
@@ -0,0 +1,28 @@
+const restaurantName = "The Royal Lounge  ";
+const cuisineType = "American";
+const cityLocation = "Chicago";
+let dailyDish = "Black Angus Steak";
+let availableTables = 12;
+let restaurantOpenClosed = "open";
+
+console.log(
+  "Restaurant Report: " +
+    "\n" +
+    "Restaurant Name: " +
+    restaurantName +
+    "\n" +
+    "Cuisine: " +
+    cuisineType +
+    "\n" +
+    "Location: " +
+    cityLocation +
+    "\n" +
+    "Today's Special: " +
+    dailyDish +
+    "\n" +
+    "Number of Tables Available: " +
+    availableTables +
+    "\n" +
+    "Status: " +
+    restaurantOpenClosed,
+);
