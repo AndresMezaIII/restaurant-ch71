f3d22a6300d0ecf89ea7ea9e00fe17d2bf54a2ed
Andres M <314112705+AndresMezaIII@users.noreply.github.com>
2026-09-06 20:38:38 -0500

    assignment 4 prototype

--- scripts/assignment4.js
+++ scripts/assignment4.js
@@ -1,5 +1,3 @@
-console.log("Assignment 4 JS linked.")
-
 const RESTAURANTNAME = "The Royal Lounge";
 
 const DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
@@ -29,3 +27,30 @@ function checkDelivery(total) {
     console.log("Delivery fees apply.\n\n");
   }
 }
+
+function getDayLabel(sales) {
+  if (sales >= 600) {
+    return label = "Excellent day";
+  } else if (sales < 600 && sales >= 400) {
+    return label = "Good day";
+  } else if (sales < 400 && sales > 250) {
+    return label = "Average day";
+  } else {
+    return label = "Slow day";
+  }
+}
+
+function generateReport() {
+  let output = "";
+  let total = 0;
+  for (i = 0; i < DAYS.length; i++) {
+    total += SALES[i];
+    console.log(total);
+    sales = SALES[i];
+    console.log(getDayLabel(sales));
+    averageSales = total / SALES.length;
+    console.log(averageSales.toFixed(2));
+  }
+}
+
+generateReport();
